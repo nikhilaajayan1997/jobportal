eeff03f6f4222eea4b4a19895fbac3454ddcd425
Nikhila Ajayan <nikhilaajayan76@gmail.com>
2022-06-09 22:39:15 +0530

    New added files

---
+++ models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Jobs(models.Model):
+    job_title_name=models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150)
+    location = models.CharField(max_length=120)
+    salary=models.PositiveIntegerField(null=True)
+    experience=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.job_title_name
